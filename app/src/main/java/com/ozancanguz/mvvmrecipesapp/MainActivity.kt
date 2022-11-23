541dbc7cbc28a6193e1484a95a8bed76b9ae0270
Ozan <ozancangu24@gmail.com>
2022-11-23 11:52:49 +0300

    -navigation component setup -bottom navigation designed.

--- app/src/main/java/com/ozancanguz/mvvmrecipesapp/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/mvvmrecipesapp/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.ozancanguz.mvvmrecipesapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
+//API Key: 97dd239ca98e45d38e09b1982f6552c6
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipes,
+                R.id.foodJokeFragment
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
