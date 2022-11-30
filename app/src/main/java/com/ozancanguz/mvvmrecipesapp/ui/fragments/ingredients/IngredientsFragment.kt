f5ec31b9d9792ca9f0d9454a5fadb10da83330f1
Ozan <ozancangu24@gmail.com>
2022-11-30 21:49:36 +0300

    -Ingredients displayed in fragment

--- app/src/main/java/com/ozancanguz/mvvmrecipesapp/ui/fragments/ingredients/IngredientsFragment.kt
+++ app/src/main/java/com/ozancanguz/mvvmrecipesapp/ui/fragments/ingredients/IngredientsFragment.kt
@@ -5,17 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ozancanguz.mvvmrecipesapp.Adapters.IngredientsAdapter
 import com.ozancanguz.mvvmrecipesapp.R
+import com.ozancanguz.mvvmrecipesapp.models.Result
+import kotlinx.android.synthetic.main.fragment_ingredients.*
+import kotlinx.android.synthetic.main.fragment_ingredients.view.*
 
 
 class IngredientsFragment : Fragment() {
+
+    private val mAdapter:IngredientsAdapter by lazy {IngredientsAdapter()}
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ingredients, container, false)
+        val view= inflater.inflate(R.layout.fragment_ingredients, container, false)
+
+        val args=arguments
+        val myBundle: Result? = args?.getParcelable<Result>("recipeBundle")
+
+        setupRv(view)
+        myBundle?.extendedIngredients.let {
+            if (it != null) {
+                mAdapter.setData(it)
+            }
+        }
+
+
+    return view
+    }
+
+    fun setupRv(view:View){
+      view. ingredients_recyclerview.layoutManager=LinearLayoutManager(requireContext())
+       view. ingredients_recyclerview.adapter=mAdapter
     }
 
 
