42b2eeb692bac5268e5fda20306ba5e9c5bf8fdf
Ozan <ozancangu24@gmail.com>
2022-11-30 21:28:59 +0300

    Modified RecipesDiffUtil.kt class

--- app/src/main/java/com/ozancanguz/mvvmrecipesapp/util/RecipesDiffUtil/RecipesDiffUtil.kt
+++ app/src/main/java/com/ozancanguz/mvvmrecipesapp/util/RecipesDiffUtil/RecipesDiffUtil.kt
@@ -3,8 +3,8 @@ package com.ozancanguz.mvvmrecipesapp.util.RecipesDiffUtil
 import androidx.recyclerview.widget.DiffUtil
 import com.ozancanguz.mvvmrecipesapp.models.Result
 
-class RecipesDiffUtil( private val oldList:List<Result>
-,  private val newList:List<Result>
+class RecipesDiffUtil<T>( private val oldList:List<T>
+,  private val newList:List<T>
 ):DiffUtil.Callback(){
     override fun getOldListSize(): Int {
 
