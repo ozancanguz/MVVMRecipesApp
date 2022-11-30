446ca724229409a0d52161639d6e23eb1799b67b
Ozan <ozancangu24@gmail.com>
2022-11-30 22:32:38 +0300

    -Modified LocalDataSource.kt class for favorite recipes

--- app/src/main/java/com/ozancanguz/mvvmrecipesapp/data/LocalDataSource.kt
+++ app/src/main/java/com/ozancanguz/mvvmrecipesapp/data/LocalDataSource.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.mvvmrecipesapp.data
 
+import com.ozancanguz.mvvmrecipesapp.data.database.Entities.FavoritesEntity
 import com.ozancanguz.mvvmrecipesapp.data.database.RecipesDao
 import com.ozancanguz.mvvmrecipesapp.data.database.Entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
@@ -9,12 +10,35 @@ class LocalDataSource @Inject constructor(
     private val recipesDao: RecipesDao
 ) {
 
+    //list all recipes
     fun readDatabase(): Flow<List<RecipesEntity>> {
         return recipesDao.readRecipes()
     }
 
+    //insert recipes
     suspend fun insertRecipes(recipesEntity: RecipesEntity) {
         recipesDao.insertRecipes(recipesEntity)
     }
+
+    // list favorites recipe
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>> {
+        return recipesDao.readFavoriteRecipes()
+    }
+
+    // insert favorite recipes
+    suspend fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity) {
+        recipesDao.insertFavoriteRecipe(favoritesEntity)
+    }
+
+    // delete favorite recipes
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity) {
+        recipesDao.deleteFavoriteRecipe(favoritesEntity)
+    }
+
+    // delete all favorite recipes
+    suspend fun deleteAllFavoriteRecipes() {
+        recipesDao.deleteAllFavoriteRecipes()
+    }
+
 
 }
