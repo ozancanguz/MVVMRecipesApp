e1ac2ad5996e2ea004370f13bba144cd3c41db18
Ozan <ozancangu24@gmail.com>
2022-11-30 15:35:36 +0300

    -pageradapter created -in detailsActivity we showed the viewpager

--- app/src/main/java/com/ozancanguz/mvvmrecipesapp/ui/DetailsActivity.kt
+++ app/src/main/java/com/ozancanguz/mvvmrecipesapp/ui/DetailsActivity.kt
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
+import com.ozancanguz.mvvmrecipesapp.Adapters.PagerAdapter
 import com.ozancanguz.mvvmrecipesapp.R
+import com.ozancanguz.mvvmrecipesapp.ui.fragments.ingredientsFragment.IngredientsFragment
+import com.ozancanguz.mvvmrecipesapp.ui.fragments.instructionsFragment.InstructionsFragment
+import com.ozancanguz.mvvmrecipesapp.ui.fragments.overviewFragment.OverviewFragment
 import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
+    private val args by navArgs<DetailsActivityArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
@@ -15,6 +23,30 @@ class DetailsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+       val fragments=ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles=ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+
+        val resultBundle=Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        val adapter=PagerAdapter(
+            resultBundle,
+            fragments,
+            titles,
+            supportFragmentManager
+
+        )
+        viewPager.adapter=adapter
+        tabLayout.setupWithViewPager(viewPager)
 
 
     }
