baee84b888d4fc67e081c0849dc8a3638bb95721
Ozan <ozancangu24@gmail.com>
2022-11-30 22:54:25 +0300

    -modified RecipesDatabase.kt class

--- app/src/main/java/com/ozancanguz/mvvmrecipesapp/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/ozancanguz/mvvmrecipesapp/data/database/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package com.ozancanguz.mvvmrecipesapp.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.ozancanguz.mvvmrecipesapp.data.database.Entities.FavoritesEntity
 import com.ozancanguz.mvvmrecipesapp.data.database.Entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
+    entities = [RecipesEntity::class,FavoritesEntity::class],
     version = 1,
     exportSchema = false
 )
