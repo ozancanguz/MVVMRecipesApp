f0125fb0c4dc27fe878980f8897667d3b4657628
Ozan <ozancangu24@gmail.com>
2022-11-30 16:28:10 +0300

    -Overview fragment completed

--- app/src/main/java/com/ozancanguz/mvvmrecipesapp/Adapters/BindingAdapters/RecipeRowBinding.kt
+++ app/src/main/java/com/ozancanguz/mvvmrecipesapp/Adapters/BindingAdapters/RecipeRowBinding.kt
@@ -11,6 +11,7 @@ import coil.load
 import com.ozancanguz.mvvmrecipesapp.R
 import com.ozancanguz.mvvmrecipesapp.models.Result
 import com.ozancanguz.mvvmrecipesapp.ui.fragments.recipes.RecipesFragmentDirections
+import org.jsoup.Jsoup
 
 
 class RecipesRowBinding {
@@ -73,6 +74,15 @@ class RecipesRowBinding {
                 }
             }
         }
+        @BindingAdapter("parseHtml")
+        @JvmStatic
+        fun parseHtml(textView: TextView, description: String?){
+            if(description != null) {
+                val desc = Jsoup.parse(description).text()
+                textView.text = desc
+            }
+        }
+
 
     }
 
