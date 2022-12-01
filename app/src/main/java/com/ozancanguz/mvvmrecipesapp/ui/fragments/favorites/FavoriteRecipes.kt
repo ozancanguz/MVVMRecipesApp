1acec560027d3fa632f327fcb075fbab2c65d417
Ozan <ozancangu24@gmail.com>
2022-12-01 18:17:33 +0300

    -modified favorite recipe fragment

--- app/src/main/java/com/ozancanguz/mvvmrecipesapp/ui/fragments/favorites/FavoriteRecipes.kt
+++ app/src/main/java/com/ozancanguz/mvvmrecipesapp/ui/fragments/favorites/FavoriteRecipes.kt
@@ -5,19 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ozancanguz.mvvmrecipesapp.R
+import com.ozancanguz.mvvmrecipesapp.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_favorite_recipes.view.*
 
-
+@AndroidEntryPoint
 class FavoriteRecipes : Fragment() {
 
+
+    private lateinit var  mainViewModel:MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        val view= inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+
+        mainViewModel=ViewModelProvider(this).get(MainViewModel::class.java)
+
+
+
+    return view
     }
+
 
 
 }
